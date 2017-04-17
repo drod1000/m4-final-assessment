fa289113bcf36af1f293fdd3704590ddb9400ad1
Daniel Rodriguez <drod2793@gmail.com>
2017-04-17 09:14:45 -0600

    Adds test for mark as unread

---
+++ spec/features/mark_as_unread_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "can mark links as unread", :js => :true do
+  scenario "Mark a link as unread" do
+    user = create(:user)
+    link = create(:link, user: user, read: true)
+
+    page.set_rack_session(user_id: user.id)
+
+    visit links_path
+
+    within('.link .read-status') do
+      expect(page).to have_text("true")
+    end
+
+    click_on "Mark as Unread"
+
+    sleep(1)
+
+    within('.link .read-status') do
+      expect(page).to have_text("false")
+    end
+
+  end
+end
