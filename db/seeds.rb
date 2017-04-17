650287835a2c40febd75615ebddbf7dfb9d43b8f
Daniel Rodriguez <drod2793@gmail.com>
2017-04-17 17:39:34 -0600

    Adds seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+user = User.create(email_address: 'd@d.com', password: 'd', password_confirmation: 'd')
+
+user.links.create(title: 'Hottest URL', url: 'http://hoturl.com', read: false)
+
+3.times do |i|
+  user.links.create(title: "Top URL #{i}", url: "http://topurl#{i}.com", read: true)
+end
+
+6.times do |i|
+  if i % 2 == 0
+    user.links.create(title: "Random URL #{i}", url: "http://someurl#{i}.com", read: false)
+  else
+    user.links.create(title: "Random URL #{i}", url: "http://someurl#{i}.com", read: true)
+  end
+end
