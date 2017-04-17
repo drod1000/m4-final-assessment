afdf751f7578cb4566e7e3002c720d6f436f394f
Daniel Rodriguez <drod2793@gmail.com>
2017-04-17 10:25:02 -0600

    Adds test for showing read only

---
+++ spec/features/filter_read_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'can filter read links', js: true do
+  scenario 'filter to see only read links' do
+    user = create(:user)
+    link_1 = user.links.create(title: 'First Title', url: 'http://firsturl.com', read: true)
+    link_2 = user.links.create(title: 'Second Title', url: 'http://secondurl.com', read: false)
+
+    page.set_rack_session(user_id: user.id)
+
+    visit links_path
+
+    click_on 'Show Read'
+
+    sleep(1)
+
+    expect(page).to have_text('First Title')
+    expect(page).to have_text('http://firsturl.com')
+    expect(page).to_not have_text('Second Title')
+    expect(page).to_not have_text('http://secondurl.com')
+  end
+end
