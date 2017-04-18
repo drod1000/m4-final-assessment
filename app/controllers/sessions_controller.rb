c10843af02089e91684a641cd8f51fae91196e40
Daniel Rodriguez <drod2793@gmail.com>
2017-04-18 15:20:28 -0600

    Adds logic for failed user login due to wrong email address

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email_address: params[:email_address])
-    if @user.authenticate(params[:password])
+    if @user == nil
+      flash[:danger] = 'That user does not exist!'
+      render :new
+    elsif @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:success] = 'Successfully logged in!'
       redirect_to links_path
